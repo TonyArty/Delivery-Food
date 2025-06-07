83f69c57526a698c12aca9ca242151cf7118dfff
Anton K <i.openuser@yandex.ru>
2025-06-07 20:17:04 +0500

    condition modal

--- script/main.js
+++ script/main.js
@@ -2,26 +2,28 @@ const modalFunc = () => {
 	const modal = document.querySelector('.cart-modal__overlay')
 	const cartBtn = document.querySelector('#cart-button')
 
-	const openModal = () => {
-		modal.classList.add('open')
-	}
-
-	const closeModal = () => {
-		modal.classList.remove('open')
-	}
-
-	cartBtn.addEventListener('click', () => {
-		openModal()
-	})
-
-	modal.addEventListener('click', (event) => {
-		if(
-			event.target.classList.contains('cart-modal__overlay') || 
-			event.target.closest('.cart-modal__header--close')
-		) {
-			closeModal()
+	if (modal) {
+		const openModal = () => {
+			modal.classList.add('open')
 		}
-	})
+
+		const closeModal = () => {
+			modal.classList.remove('open')
+		}
+
+		cartBtn.addEventListener('click', () => {
+			openModal()
+		})
+
+		modal.addEventListener('click', (event) => {
+			if(
+				event.target.classList.contains('cart-modal__overlay') || 
+				event.target.closest('.cart-modal__header--close')
+			) {
+				closeModal()
+			}
+		})
+	}
 }
 
 const restsFunc = () => {
